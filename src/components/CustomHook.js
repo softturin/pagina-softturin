6c183a5ef35fe5579e0704d6c3bfa2fd7a737c5c
juancho250 <juaz2560@gmail.com>
2024-05-30 16:29:51 -0500

    ya

--- src/components/CustomHook.js
+++ src/components/CustomHook.js
@@ -1,34 +1,41 @@
-import { useEffect } from 'react' 
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 const CustomHook = (refTab = null, refList = null) => {
-const scrollTab = refTab;
-const divs = refList;
-const activeTab = useSelector(state => state.activeTab);
+    const scrollTab = refTab;
+    const divs = refList;
+    const activeTab = useSelector(state => state.activeTab);
 
-useEffect(() => {
-    if(scrollTab.current.classList.contains(activeTab)){
-        const componentNode = scrollTab.current;
-        componentNode.scrollIntoView({ behavior: 'smooth' });
-    }
-    if(divs !== null){
-        divs.current.forEach((div) => {
-            div.classList.add('animation');
-          });
-        const handlScroll = () => {
-          const scrollPosition = window.scrollY;
-          divs.current.forEach((div) => {
-            const offsetTop = div.getBoundingClientRect().top + scrollPosition;
-            if (scrollPosition >= offsetTop - (window.innerHeight / (1.5))) {
-              div.classList.add('active');
-            }else{
-              div.classList.remove('active');
-            }
-          });
+    useEffect(() => {
+        if(scrollTab && scrollTab.current && scrollTab.current.classList.contains(activeTab)){
+            const componentNode = scrollTab.current;
+            componentNode.scrollIntoView({ behavior: 'smooth' });
         }
-          window.addEventListener('scroll', handlScroll);
-    }
-}, [activeTab])
-}
+        if(divs !== null && divs.current){
+            divs.current.forEach((div) => {
+                div.classList.add('animation');
+            });
+            const handlScroll = () => {
+                const scrollPosition = window.scrollY;
+                divs.current.forEach((div) => {
+                    const offsetTop = div.getBoundingClientRect().top + scrollPosition;
+                    if (scrollPosition >= offsetTop - (window.innerHeight / (1.5))) {
+                        div.classList.add('active');
+                    } else {
+                        div.classList.remove('active');
+                    }
+                });
+            };
+            window.addEventListener('scroll', handlScroll);
 
-export default CustomHook
+            return () => {
+                // Limpiar el listener del evento scroll cuando el componente se desmonte
+                window.removeEventListener('scroll', handlScroll);
+            };
+        }
+    }, [activeTab, divs, scrollTab]);
+
+    // Si necesitas devolver algo desde el hook, puedes hacerlo aquí.
+};
+
+export default CustomHook;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react' 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faHtml5, faCss3, faJs, faVuejs, faLaravel, faNode, faPhp, faPython} from '@fortawesome/free-brands-svg-icons';
+import { faReact, faHtml5, faCss3, faNode, faPhp, faPython} from '@fortawesome/free-brands-svg-icons';
 import CustomHook from './CustomHook';
 
 function Skills() {
