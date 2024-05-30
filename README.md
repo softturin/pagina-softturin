ab71f0b0116bc1ea80458c750c28c7d25d00def8
juancho250 <juaz2560@gmail.com>
2024-05-30 16:48:07 -0500

    ya

--- README.md
+++ README.md
@@ -11,4 +11,4 @@ Step 2:
 ```
 npm start
 ```
-This will launch the development server and open your browser, displaying the application at http://localhost:3000/ (or another port if port 3000 is already in use).# Softturin
+This will launch the development server and open your browser, displaying the application at http://localhost:3000/ (or another port if port 3000 is already in use).
--- src/components/CustomHook.js
+++ src/components/CustomHook.js
@@ -1,41 +1,34 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react' 
 import { useSelector } from 'react-redux';
 
 const CustomHook = (refTab = null, refList = null) => {
-    const scrollTab = refTab;
-    const divs = refList;
-    const activeTab = useSelector(state => state.activeTab);
+const scrollTab = refTab;
+const divs = refList;
+const activeTab = useSelector(state => state.activeTab);
 
-    useEffect(() => {
-        if(scrollTab && scrollTab.current && scrollTab.current.classList.contains(activeTab)){
-            const componentNode = scrollTab.current;
-            componentNode.scrollIntoView({ behavior: 'smooth' });
+useEffect(() => {
+    if(scrollTab.current.classList.contains(activeTab)){
+        const componentNode = scrollTab.current;
+        componentNode.scrollIntoView({ behavior: 'smooth' });
+    }
+    if(divs !== null){
+        divs.current.forEach((div) => {
+            div.classList.add('animation');
+          });
+        const handlScroll = () => {
+          const scrollPosition = window.scrollY;
+          divs.current.forEach((div) => {
+            const offsetTop = div.getBoundingClientRect().top + scrollPosition;
+            if (scrollPosition >= offsetTop - (window.innerHeight / (1.5))) {
+              div.classList.add('active');
+            }else{
+              div.classList.remove('active');
+            }
+          });
         }
-        if(divs !== null && divs.current){
-            divs.current.forEach((div) => {
-                div.classList.add('animation');
-            });
-            const handlScroll = () => {
-                const scrollPosition = window.scrollY;
-                divs.current.forEach((div) => {
-                    const offsetTop = div.getBoundingClientRect().top + scrollPosition;
-                    if (scrollPosition >= offsetTop - (window.innerHeight / (1.5))) {
-                        div.classList.add('active');
-                    } else {
-                        div.classList.remove('active');
-                    }
-                });
-            };
-            window.addEventListener('scroll', handlScroll);
+          window.addEventListener('scroll', handlScroll);
+    }
+}, [activeTab])
+}
 
-            return () => {
-                // Limpiar el listener del evento scroll cuando el componente se desmonte
-                window.removeEventListener('scroll', handlScroll);
-            };
-        }
-    }, [activeTab, divs, scrollTab]);
-
-    // Si necesitas devolver algo desde el hook, puedes hacerlo aquí.
-};
-
-export default CustomHook;
+export default CustomHook
